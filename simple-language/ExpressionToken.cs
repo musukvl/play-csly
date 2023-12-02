e774f3241163c993f3205f21201bcacfee369827
Alexey Ryazhskikh <musukvl@gmail.com>
2023-12-03 01:41:58 +0200

    Inital commit for simple language 2+1 - 2 expressions

---
+++ simple-language/ExpressionToken.cs
@@ -0,0 +1,20 @@
+﻿using sly.lexer;
+
+namespace MyParsers;
+
+public enum ExpressionToken {
+
+    //[Lexeme("[0-9]+")]
+    [Lexeme("[0-9]+")]
+    INT = 1,
+
+    [Lexeme("\\+")] 
+    PLUS = 2,
+    
+    [Lexeme("\\-")] 
+    MINUS = 3,
+
+    [Lexeme("[ \\t]+", isSkippable:true)] // the lexeme is marked isSkippable : it will not be sent to the parser and simply discarded.
+    WS = 40 
+
+}
---
+++ simple-language/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using MyParsers;
+
+string expression = "42  + 42 - 7";
+
+var parser = MyFirstParser.GetParser();
+var parseResult = parser.Parse(expression);
+
+
+
+if (!parseResult.IsError) 
+{
+    Console.WriteLine($"result of <{expression}>  is {(int)parseResult.Result}");
+    // outputs : result of <42 + 42>  is 84"
+}
+else
+{
+    if (parseResult.Errors != null && parseResult.Errors.Any())
+    {
+        // display errors
+        parseResult.Errors.ForEach(error => Console.WriteLine(error.ErrorMessage));
+    }
+}
+
+Console.WriteLine("Hello, World!");
